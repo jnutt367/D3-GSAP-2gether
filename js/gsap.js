876e85e17427386de80b2d9fcc972c70656625a7
Jason Nutt <jnutt367@gmail.com>
2021-04-19 18:05:08 -0400

    commit newest for experiment

--- js/gsap.js
+++ js/gsap.js
@@ -1,1 +1,2 @@
-gsap.to('svg', { scale: "1.2", x: 50, y: 300 })
+gsap.to('svg', { duration: 2, scale: "1.2", x: 50, y: 300, ease: "bounce" })
+gsap.to('p', { duration: 2, scale: 1.1, backgroundColor: "black", padding: "15", borderRadius: "5px", margin: "20" })
