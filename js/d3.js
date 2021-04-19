454f86741b22fb9697aa191fab543476681bfbfa
Jason Nutt <jnutt367@gmail.com>
2021-04-19 17:44:34 -0400

    commit first try at gsap and d3 together

---
+++ js/d3.js
@@ -0,0 +1,25 @@
+const DUMMY_DATA = [
+    { id: 'd1', value: 9, region: 'Rivendale' },
+    { id: 'd2', value: 12, region: 'Gondor' },
+    { id: 'd3', value: 7, region: 'Ministirith' },
+    { id: 'd4', value: 4, region: 'Germany' },
+    { id: 'd3', value: 20, region: 'USA' }
+];
+
+const xScale = d3.scaleBand().domain(DUMMY_DATA.map(dataPoint => dataPoint.region)).rangeRound([0, 250]).padding(0.3);
+const yScale = d3.scaleLinear().domain([0, 30]).range([200, 0]);
+
+
+const container = d3.select('svg')
+    .classed('container', true);
+
+const bars = container
+    .selectAll('.bar')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', xScale.bandwidth())
+    .attr('height', (data) => 200 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value));
---
+++ js/gsap.js
@@ -0,0 +1,1 @@
+gsap.to('svg', { scale: "1.2", x: 50, y: 300 })
