778cc3b7f5f2302d707408514735bd2bb8fc55d5
Jason Nutt <jnutt367@gmail.com>
2021-04-20 13:44:00 -0400

    commit for new svg and bottom animation

--- js/gsap.js
+++ js/gsap.js
@@ -11,10 +11,12 @@ let tl = gsap.timeline({
             scrub: 1
         }
     })
-    .to('.svg', { duration: 2, scale: "1.2", x: 50, y: 300, ease: "bounce" })
-    .to('p', { duration: 2, scale: 1.1, textContent: "A tables's TITLE? OR WHATEVER", backgroundColor: "black", padding: "15", borderRadius: "5px", margin: "30" })
+    .to('.svg', { duration: 2, scale: "1.2", x: 50, y: 300, ease: "bounce", position: "relative", zIndex: 2 })
+    .to('p', { duration: 2, scale: 1.1, textContent: "A tables's TITLE? OR WHATEVER", backgroundColor: "black", padding: "15", borderRadius: "5px" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(red, blue, green)" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(#123, #432, #555)", backgroundSize: "contain" })
     .to('body', { duration: 2, backgroundImage: "radial-gradient(red, blue, green)", backgroundSize: "contain" })
-    .to('h1', { color: "white", fontSize: "35", marginTop: "400" })
+    .to('h1', { color: "white", fontSize: "35" })
     .to('h2', { duration: 5, color: "aliceblue" })
+    .to('.blue-circle', { duration: 10, scale: 1.3, y: -150, ease: "elastic", color: "aliceblue" })
+    .to('.green-dot', { duration: "3", scale: 20, y: -200, zIndex: 10, position: "relative" })
