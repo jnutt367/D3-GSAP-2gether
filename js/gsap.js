82e587f8b6cdec2cee493a4839a8e8ea9d1d2c54
Jason Nutt <jnutt367@gmail.com>
2021-04-20 14:22:11 -0400

    commit for new blue svg at bottom

--- js/gsap.js
+++ js/gsap.js
@@ -3,15 +3,16 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.fromTo('h3', { x: 200, opacity: 0 }, { duration: 4, x: 50, y: 100, fontFamily: "fantasy", opacity: 1, color: "blue", fontSize: "2em", ease: "elastic" });
 
 let tl = gsap.timeline({
+        duaration: 10,
         scrollTrigger: {
-            trigger: "p",
+            trigger: ".svg",
             start: "top 70%",
             end: "bottom 40%",
             markers: true,
             scrub: 1
         }
     })
-    .to('.svg', { duration: 2, scale: "1.2", x: 50, y: 300, ease: "bounce", position: "relative", zIndex: 2 })
+    .to('.svg', { duration: 7, scale: "1.2", x: 50, y: 300, ease: "bounce", position: "relative", zIndex: 2 })
     .to('p', { duration: 2, scale: 1.1, textContent: "A tables's TITLE? OR WHATEVER", backgroundColor: "black", padding: "15", borderRadius: "5px" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(red, blue, green)" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(#123, #432, #555)", backgroundSize: "contain" })
