7d08f0571a3a4b22d31c336b2ef8df5657892b1f
Jason Nutt <jnutt367@gmail.com>
2021-04-20 12:40:56 -0400

    commit for textcontent chenge for p tag

--- js/gsap.js
+++ js/gsap.js
@@ -12,7 +12,7 @@ let tl = gsap.timeline({
         }
     })
     .to('svg', { duration: 2, scale: "1.2", x: 50, y: 300, ease: "bounce" })
-    .to('p', { duration: 2, scale: 1.1, backgroundColor: "black", padding: "15", borderRadius: "5px", margin: "30" })
+    .to('p', { duration: 2, scale: 1.1, textContent: "A tables's TITLE? OR WHATEVER", backgroundColor: "black", padding: "15", borderRadius: "5px", margin: "30" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(red, blue, green)" })
     .to('body', { duration: 2, backgroundImage: "linear-gradient(#123, #432, #555)", backgroundSize: "contain" })
     .to('body', { duration: 2, backgroundImage: "radial-gradient(red, blue, green)", backgroundSize: "contain" })
